eab8c45ec13b7f1931d4adfa49c1c61ce0598280
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2015-03-28 10:29:29 +0100

    finished assignment 7

--- mlclass-ex7-008/mlclass-ex7/projectData.m
+++ mlclass-ex7-008/mlclass-ex7/projectData.m
@@ -9,6 +9,9 @@ function Z = projectData(X, U, K)
 % You need to return the following variables correctly.
 Z = zeros(size(X, 1), K);
 
+m = size(X,1);
+U_reduce = U(:, 1:K);
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the projection of the data using only the top K 
 %               eigenvectors in U (first K columns). 
@@ -18,8 +21,10 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
-
-
+for i =1:m
+	x = X(i, :);
+	Z(i,:) = x * U_reduce;
+end;
 
 % =============================================================
 
