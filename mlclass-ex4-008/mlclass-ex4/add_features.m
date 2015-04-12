779d93b05667e59bdfff24dd56e23b5afe0b03e4
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2015-04-12 19:07:40 +0200

    vectorized add_features.m

--- mlclass-ex4-008/mlclass-ex4/add_features.m
+++ mlclass-ex4-008/mlclass-ex4/add_features.m
@@ -19,39 +19,54 @@ function [Xreturn Y] = add_features(X)
 
 	%% power2-products (except quadratic)
 
-	Xpower2 = zeros(m, 90);
-	for row=1:m
-		column = 1;
-		for i=1:9
-			for j=(i+1):10
-				product = X(row,ix(i))*X(row,ix(j));
-				Xpower2(row, column) = product;
-				column = column+1;
-				quotient = 0;
-				if product>0
-					quotient = X(row,ix(i))/X(row,ix(j));
-				end
-				Xpower2(row, column) = quotient;
-				column = column+1;
-			end
+	% Xpower2 = zeros(m, 90);
+	% for row=1:m
+	% 	column = 1;
+	% 	for i=1:9
+	% 		for j=(i+1):10
+	% 			product = X(row,ix(i))*X(row,ix(j));
+	% 			Xpower2(row, column) = product;
+	% 			column = column+1;
+	% 			quotient = 0;
+	% 			if product>0
+	% 				quotient = X(row,ix(i))/X(row,ix(j));
+	% 			end
+	% 			Xpower2(row, column) = quotient;
+	% 			column = column+1;
+	% 		end
+	% 	end
+	% end
+
+	for i=1:9
+		for j=(i+1):10
+			X = [X X(:,ix(i)).*X(:,ix(j))];
+			X = [X X(:,ix(i))./(1+X(:,ix(j)))];
 		end
 	end
 
 	%% power3-products
 
-	Xpower3 = zeros(m, 64);
-	for row=1:m
-		column = 1;
-		for i=1:4
-			for j=1:4
-				for k=1:4
-					Xpower3(row, column) = X(row,ix(i))*X(row,ix(j))*X(row,ix(k));
-					column = column+1;
-				end
+	% Xpower3 = zeros(m, 64);
+	% for row=1:m
+	% 	column = 1;
+	% 	for i=1:4
+	% 		for j=1:4
+	% 			for k=1:4
+	% 				Xpower3(row, column) = X(row,ix(i))*X(row,ix(j))*X(row,ix(k));
+	% 				column = column+1;
+	% 			end
+	% 		end
+	% 	end
+	% end
+
+	for i=1:4
+		for j=1:4
+			for k=1:4
+				X = [X X(:,ix(i)).*X(:,ix(j)).*X(:,ix(k))]
 			end
 		end
 	end
 
-	Xreturn = [X X2 Xpower2 Xpower3];
+	Xreturn = [X X2];
 
 end
